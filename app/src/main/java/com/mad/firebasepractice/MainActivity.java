ea4ebe6a3fbc8a2c44b243ade1c55b230458f996
MittyBoySour <mitchell_creecy@live.com>
2018-05-27 15:58:20 +1000

    updated recyclerview in main

--- app/src/main/java/com/mad/firebasepractice/MainActivity.java
+++ app/src/main/java/com/mad/firebasepractice/MainActivity.java
@@ -2,85 +2,141 @@ package com.mad.firebasepractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String USER_FILTER = "user";
+    private static final String USER_MAIL_FILTER = "mail";
+    private User currentUser;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private RecyclerView mMailRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        DatabaseReference mailReference = FirebaseDatabase.getInstance()
-                .getReference("user"); // will be user on initialisation
+        // setup views
+        mMailRecyclerView = (RecyclerView) findViewById(R.id.mail_list_recycler);
+        //mLinearLayoutManager.setStackFromEnd(true);
+        mMailRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        String userKey = mailReference.push().getKey();
-        Log.d(TAG, "onCreate: user: " + userKey);
+        // setup DB and references
+        mDatabaseReference = FirebaseDatabase.getInstance()
+                .getReference();
+        setupTestTree();
+        // currentUser = new User("");
+        DatabaseReference userMailQuery = mDatabaseReference.child(USER_FILTER)
+                .child(currentUser.userId).child(USER_MAIL_FILTER);
+        Log.d(TAG, "onCreate: userMailQuery.toString(): " + userMailQuery.toString());
 
-        writeNewUser(userKey,"Mitty", "mitty@mitty.com",
-                FirebaseDatabase.getInstance().getReference());
+        // setup recycler
+        SnapshotParser<Mail> parser = getMailSnapshotParser();
+        FirebaseRecyclerOptions<Mail> options = getMailRecyclerOptions(userMailQuery, parser);
+        // TODO: move with viewholder to new class
+        mFirebaseAdapter = getFirebasebaseRA(options);
+        mMailRecyclerView.setAdapter(mFirebaseAdapter);
 
-        updateUsername(userKey, "Billy",
-                FirebaseDatabase.getInstance().getReference());
+    }
 
-        writeNewPost(userKey, "Billy", "Titled as hell",
-                "so much text ",
-                FirebaseDatabase.getInstance().getReference());
+    private void setupTestTree() {
+        User testUser1 = new User("TestUser1");
+        writeNewUser(testUser1);
+        User testUser2 = new User("TestUser2");
+        writeNewUser(testUser2);
+        addUserToContacts(testUser1, testUser2, "TestFriend2");
+        Mail mailFromUser1 = new Mail(testUser1.userId, testUser2.userId,
+                "Mail 1 title", "Mail 1 message");
+        sendNewMailToUser(mailFromUser1);
+        addUserToContacts(testUser2, testUser1, "TestFriend1");
+        Mail mailFromUser2 = new Mail(testUser2.userId, testUser1.userId,
+                "Re: Mail 1 title", "Thank you for Mail 1");
+        sendNewMailToUser(mailFromUser2);
+        currentUser = testUser1;
+    }
 
-        // Read from the database
-        mailReference.addValueEventListener(new ValueEventListener() {
+    private void writeNewUser(User user) {
+        // assumes check for userName validity has been performed
+        mDatabaseReference.child("user").setValue(user);
+    }
+
+    private void addUserToContacts(User user, User contact, String nickname) {
+        mDatabaseReference.child("user").child(user.userId)
+                .child("contact").child(contact.userId)
+                .child("nickname").setValue(nickname);
+    }
+
+    private void sendNewMailToUser(Mail mail) {
+        if (mail.recipientId.equalsIgnoreCase("")) { return; }
+        String mailKey = mDatabaseReference.child("user").child(mail.recipientId)
+                .child("mail").push().getKey();
+        mDatabaseReference.child("user").child(mail.recipientId)
+                .child("mail").child(mailKey).setValue(mail); // check that user still exists first
+    }
+
+    private SnapshotParser<Mail> getMailSnapshotParser() {
+        return new SnapshotParser<Mail>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+            public Mail parseSnapshot(DataSnapshot dataSnapshot) {
+                Mail mail = dataSnapshot.getValue(Mail.class);
+                return mail;
+            }
+        };
+    }
+
+    private FirebaseRecyclerOptions<Mail> getMailRecyclerOptions(
+            DatabaseReference query, SnapshotParser<Mail> parser) {
+        return new FirebaseRecyclerOptions.Builder<Mail>()
+                .setQuery(query, parser)
+                .build();
+    }
+
+    private FirebaseRecyclerAdapter<Mail, MailViewHolder> getFirebasebaseRA(
+            FirebaseRecyclerOptions<Mail> options) {
+        return new FirebaseRecyclerAdapter<Mail, MailViewHolder>(options) {
+            @Override
+            public MailViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                return new MailViewHolder(inflater.inflate(R.layout.mail_item, viewGroup,
+                        false));
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            protected void onBindViewHolder(final MailViewHolder viewHolder,
+                                            int position,
+                                            Mail mail) {
+                viewHolder.titleTv.setText(mail.mailTitle);
+                viewHolder.senderTV.setText(mail.senderId);
             }
-        });
+        };
     }
 
-    private void writeNewUser(String userId, String name, String email,
-                              DatabaseReference database) {
-        User user = new User(name, email);
+    private class MailViewHolder extends RecyclerView.ViewHolder {
 
-        database.child("users").child(userId).setValue(user);
+        public TextView senderTV;
+        public TextView titleTv;
+        // make separate views for status
+
+        public MailViewHolder(View view) {
+            super(view);
+            senderTV = findViewById(R.id.mail_sender);
+            titleTv = findViewById(R.id.mail_title);
+        }
     }
-
-    private void updateUsername(String userId, String newName, DatabaseReference database) {
-        database.child("users").child(userId).child("username").setValue(newName);
-
-    }
-    private void writeNewPost(String userId, String username, String title, String body,
-                              DatabaseReference databaseReference) {
-        // Create new post at /user-posts/$userid/$postid and at
-        // /posts/$postid simultaneously
-        String key = databaseReference.child("posts").push().getKey();
-        Post post = new Post(userId, username, title, body);
-        Map<String, Object> postValues = post.toMap();
-
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put("/posts/" + key, postValues);
-        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
-
-        databaseReference.updateChildren(childUpdates);
-    }
-
 }
